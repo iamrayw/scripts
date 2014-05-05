aec3ea452736ca66a3ad26b093c9a277363eb56f
Ray <raymond.winder@bhnetwork.com>
2014-05-05 15:42:02 -0700

    add keygen.sh

---
+++ sysadmin_tools/keygen.sh
@@ -0,0 +1,56 @@
+	#!/bin/bash 
+#test
+echo "This script is to be used for generating SSH certificates only."
+
+read -p "Bits (default: 8192): " bits
+
+	if [ -z "$bits" ]; then
+	    bits=8192
+	fi
+	 
+read -p "Encryption Type (default: rsa): " enc
+	 
+if [ -z "$enc" ]; then
+    enc="rsa"
+fi
+ 
+read -p "File (default: $HOME/.ssh/id_$enc): " path
+
+if [ -z "$path" ]; then
+    path="$HOME/.ssh/id_$enc"
+fi
+ 
+if [ -e "$path" ]; then
+    read -p "$path already exists...delete? (Y/n): " ans
+
+   case "$ans" in
+      N|n)
+          echo "File must be deleted first."
+          exit 1
+       ;;
+
+        Y|y|*)
+            rm -rf $path
+        ;;
+    esac
+fi
+ 
+stty -echo
+read -p "Passphrase: " pp
+stty echo
+
+if [ "${#pp}" -lt 4 ]; then
+    echo -e "\nPassphrase must be greater than 4 characters."
+    exit 1
+fi
+ 
+echo -e -n "\nGenerating a $bits bit $enc SSH key file in $path..."
+ 
+ssh-keygen -q -b $bits -t $enc -N $pp -f "$path"
+ 
+if [ -e "$path" ]; then
+    echo "SUCCESS"
+    else
+    echo "FAIL"
+fi 
+exit 0
