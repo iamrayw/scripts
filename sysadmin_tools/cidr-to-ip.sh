468f85c6057e9e29247818a75fa3308d61867704
iamrayw <raymondwinder@gmail.com>
2016-03-03 10:22:25 -0800

    Update cidr-to-ip.sh

--- sysadmin_tools/cidr-to-ip.sh
+++ sysadmin_tools/cidr-to-ip.sh
@@ -1,4 +1,19 @@
 #!/bin/bash    
+#USEAGE
+#./cidr-to-ip.sh [OPTION(only one)] [STRING/FILENAME]
+#OPTIONS:
+#-h Displays this help screen
+#-f Forces a check for network boundary when given a STRING(s)
+#-i Will read from an Input file (file should contain one CIDR per line)(no network boundary check)
+#-b Will do the same as -i but with network boundary check
+#EXAMPLES:
+#./cidr-to-ip.sh 192.168.0.1/24
+#./cidr-to-ip.sh 192.168.0.1/24 10.10.0.0/28
+#./cidr-to-ip.sh -f 192.168.0.0/16
+#./cidr-to-ip.sh -i inputfile.txt
+#./cidr-to-ip.sh -b inputfile.txt
+
+
 
 ############################
 ##  Methods
